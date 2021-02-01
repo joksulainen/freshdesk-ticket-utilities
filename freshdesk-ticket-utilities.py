c5fa60113bd74785fe604a0741d2ae4b727863e8
joksulainen <minna1970.mn@gmail.com>
2021-02-01 14:05:49 +0200

    Treeview functionality + GUI layout rearranging

--- freshdesk-ticket-utilities.py
+++ freshdesk-ticket-utilities.py
@@ -6,22 +6,28 @@ import tkcalendar as tkc
 # Import backend
 import backend
 
+# Prepare variable
+loadedTicket = None
+
 # Button commands
 def getTicketList():
-    pass
+    ticketlist = backend.getTicketList(1)
+    for x in range(len(ticketlist)):
+        ticket = ticketlist[x]
+        trv_ticketlist.insert("", "end", iid=x, text=ticket.id, values=(ticket.subject, ticket.status, ticket.created_at))
 
 def bulkClose():
     pass
 
 def confirmBulkClose():
-    if bulkCloseConfirm.get():
+    if confirmBulkCloseVar.get():
         btn_bulkclose.config(state="normal")
     else:
         btn_bulkclose.config(state="disabled")
 
 def getTicket():
     try:
-        loadedTicket = backend.getTicket(int(ent_ticketid.get()))
+        loadedTicket = backend.getTicket(int(ticketidVar.get()))
     except ValueError:
         return messagebox.showerror("Error", "Ticket ID is not a number")
     if loadedTicket.status != "closed":
@@ -54,7 +60,7 @@ Company ID: {loadedTicket.company['id']}""")
     lbl_description.config(text=loadedTicket.description_text)
 
 def closeTicket():
-    if backend.closeTicket(loadedTicket):
+    if backend.closeTicket(loadedTicket.id):
         btn_closeticket.config(state="disabled")
     else:
         messagebox.showerror("Problem with closing ticket", "An error has occurred while attempting to close ticket")
@@ -63,40 +69,51 @@ def closeTicket():
 win_main = tk.Tk()
 win_main.resizable(False, False)
 win_main.title("Freshdesk Ticket Utilities")
-frm_ticketlist = ttk.Frame(borderwidth='1', height='450', relief='raised', width='450')
+# Main frame
+frm_ticketlist = ttk.Frame(borderwidth=1, height=400, relief='raised', width=600)
 u0 = ttk.Label(frm_ticketlist, text='Company ID')
-u0.place(x='5', y='0')
-ent_companyid = ttk.Entry(frm_ticketlist, width='15')
-ent_companyid.place(x='5', y='20')
+u0.place(x=5, y=0)
+ent_companyid = ttk.Entry(frm_ticketlist, width=15, exportselection="no")
+ent_companyid.place(x=5, y=20)
 u1 = ttk.Label(frm_ticketlist, text='From')
-u1.place(x='105', y='0')
-ent_startdate = tkc.DateEntry(frm_ticketlist, width='12')
-ent_startdate.place(x='105', y='20')
+u1.place(x=105, y=0)
+ent_startdate = tkc.DateEntry(frm_ticketlist, locale="fi_FI", width=12)
+ent_startdate.place(x=105, y=20)
 u2 = ttk.Label(frm_ticketlist, text='Until')
-u2.place(x='205', y='0')
-ent_enddate = tkc.DateEntry(frm_ticketlist, width='12')
-ent_enddate.place(x='205', y='20')
+u2.place(x=205, y=0)
+ent_enddate = tkc.DateEntry(frm_ticketlist, locale="fi_Fi",width=12)
+ent_enddate.place(x=205, y=20)
 btn_refreshlist = ttk.Button(frm_ticketlist, text='Refresh and filter', command=getTicketList)
-btn_refreshlist.place(x='305', y='19')
+btn_refreshlist.place(x=305, y=19)
 btn_bulkclose = ttk.Button(frm_ticketlist, state='disabled', text='Close tickets on page', command=bulkClose)
-btn_bulkclose.place(x='5', y='45')
-bulkCloseConfirm = tk.BooleanVar()
-chk_confirm = ttk.Checkbutton(frm_ticketlist, offvalue='False', onvalue='True', state='normal', text='Are you sure?', variable=bulkCloseConfirm, command=confirmBulkClose)
-chk_confirm.place(x='128', y='47')
-sel_statusfilter = ttk.Combobox(frm_ticketlist, values='"" "open" "pending" "resolved" "closed"', width='13')
-sel_statusfilter.place(x='305', y='45')
-trv_ticketlist = ttk.Treeview(frm_ticketlist, height='14')
-trv_ticketlist.place(width='410', x='10', y='80')
-scrl_ticketlist = ttk.Scrollbar(frm_ticketlist, orient='vertical')
-scrl_ticketlist.place(height='307', x='420', y='80')
-frm_ticketlist.pack(anchor='n', side='left')
-frm_ticket = ttk.Frame(borderwidth='1', height='450', relief='raised', width='600')
+btn_bulkclose.place(x=5, y=45)
+confirmBulkCloseVar = tk.BooleanVar()
+chk_confirm = ttk.Checkbutton(frm_ticketlist, state='disabled', text='Are you sure?', variable=confirmBulkCloseVar, command=confirmBulkClose)
+chk_confirm.place(x=128, y=47)
+sel_statusfilter = ttk.Combobox(frm_ticketlist, values='"" "open" "pending" "resolved" "closed"', width=13)
+sel_statusfilter.place(x=305, y=45)
+# Treeview configuration
+trv_ticketlist = ttk.Treeview(frm_ticketlist, height=14, columns=("Subject", "Status", "Created"), selectmode="browse", show="headings")
+trv_ticketlist.heading("#0", text="ID")
+trv_ticketlist.heading("#1", text="Subject")
+trv_ticketlist.heading("#2", text="Status")
+trv_ticketlist.heading("#3", text="Created")
+trv_ticketlist.column("#1", stretch="yes", minwidth=200, width=200)
+trv_ticketlist.column("#2", stretch="no", minwidth=60, width=60)
+trv_ticketlist.column("#3", stretch="no", minwidth=150, width=150)
+trv_ticketlist.place(width=560, x=10, y=80)
+vsb_ticketlist = ttk.Scrollbar(frm_ticketlist, orient='vertical', command=trv_ticketlist.yview)
+trv_ticketlist.configure(yscrollcommand=vsb_ticketlist.set)
+vsb_ticketlist.place(height=307, x=570, y=80)
+frm_ticketlist.pack(anchor='nw', side='top')
+# Ticket view frame
+frm_ticket = ttk.Frame(borderwidth=1, height=410, relief='raised', width=600)
 u3 = ttk.Label(frm_ticket, text='Description')
-u3.place(x='236', y='0')
-lbl_description = ttk.Label(frm_ticket, borderwidth='1', relief='sunken', wraplength='350')
-lbl_description.place(height='420', width='355', x='236', y='20')
+u3.place(x=236, y=0)
+lbl_description = ttk.Label(frm_ticket, borderwidth=1, relief='sunken', wraplength=350)
+lbl_description.place(height=380, width=355, x=236, y=20)
 u4 = ttk.Label(frm_ticket, text='Ticket ID')
-u4.place(x='5', y='0')
+u4.place(x=5, y=0)
 lbl_details = ttk.Label(frm_ticket)
 lbl_details.configure(text='''ID: 
 Subject: 
@@ -119,15 +136,24 @@ E-Mail:
 Phone: 
 Requester ID: 
 Company: 
-Company ID: ''', wraplength='220')
-lbl_details.place(width='220', x='5', y='50')
-ent_ticketid = ttk.Entry(frm_ticket, width='10')
-ent_ticketid.place(x='5', y='20')
+Company ID: ''', wraplength=220)
+lbl_details.place(width=220, x=5, y=50)
+ticketidVar = tk.StringVar()
+ent_ticketid = ttk.Entry(frm_ticket, width=10, exportselection="no", textvariable=ticketidVar)
+ent_ticketid.place(x=5, y=20)
 btn_getticket = ttk.Button(frm_ticket, text='Get ticket', command=getTicket)
-btn_getticket.place(x='75', y='19')
+btn_getticket.place(x=75, y=19)
 btn_closeticket = ttk.Button(frm_ticket, state='disabled', text='Close ticket', command=closeTicket)
-btn_closeticket.place(x='155', y='19')
-frm_ticket.pack(anchor='n', side='left')
+btn_closeticket.place(x=155, y=19)
+frm_ticket.pack(anchor='nw', side='top')
+
+# Treeview selection
+def ticketSelected(e):
+    selectedTicket = trv_ticketlist.item(trv_ticketlist.focus())["text"]
+    ticketidVar.set(selectedTicket)
+    getTicket()
+
+trv_ticketlist.bind("<Double-Button-1>", ticketSelected)
 
 # Start the main loop which in turn displays the window
 win_main.mainloop()
