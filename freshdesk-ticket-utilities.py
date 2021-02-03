056db5e638d1c910ef3d1fb51cfd02564731a807
joksulainen <minna1970.mn@gmail.com>
2021-02-03 10:33:04 +0200

    UI tweaking, error handler for missing company ID

--- freshdesk-ticket-utilities.py
+++ freshdesk-ticket-utilities.py
@@ -1,5 +1,5 @@
 # Import dependencies
-import time
+from datetime import datetime, timedelta
 # Import tkinter toolkits
 import tkinter as tk
 import tkinter.ttk as ttk
@@ -10,6 +10,7 @@ import backend
 
 # Prepare variables
 loadedTicket = None
+loadedTicketlist = None
 companyDict = {}
 
 # Button commands
@@ -19,10 +20,16 @@ def getTicketList():
             for x in range(30):
                 trv_ticketlist.delete(x)
         except tk.TclError: pass
-    ticketlist = backend.getTicketList(1)
-    for x in range(len(ticketlist)):
-        ticket = ticketlist[x]
-        trv_ticketlist.insert("", "end", iid=x, text=ticket.id, values=("", ticket.subject, ticket.status, ticket.created_at.strftime("%d.%m.%Y")))
+    fromDate = ent_startdate.get_date().strftime("%Y-%m-%d")
+    untilDate = ent_enddate.get_date().strftime("%Y-%m-%d")
+    loadedTicketlist = backend.getTicketList(1, fromDate=fromDate, untilDate=untilDate)
+    for x in range(len(loadedTicketlist)):
+        ticket = loadedTicketlist[x]
+        try:
+            company = companyDict[ticket.company_id]
+        except KeyError:
+            company = None
+        trv_ticketlist.insert("", "end", iid=x, text=ticket.id, values=(company, ticket.subject, ticket.status, ticket.created_at.strftime("%d.%m.%Y")))
 
 def bulkClose():
     pass
@@ -43,6 +50,12 @@ def getTicket():
     else:
         btn_closeticket.config(state="disabled")
     agent = backend.getAgent(loadedTicket.responder_id)
+    try:
+        companyname = loadedTicket.company['name']
+        companyid = loadedTicket.company['id']
+    except KeyError:
+        companyname = None
+        companyid = None
     lbl_details.config(text=f"""ID: {loadedTicket.id}
 Subject: {loadedTicket.subject}
 
@@ -63,8 +76,8 @@ Requester: {loadedTicket.requester['name']}
 E-Mail: {loadedTicket.requester['email']}
 Phone: {loadedTicket.requester['phone']}
 Requester ID: {loadedTicket.requester['id']}
-Company: {loadedTicket.company['name']}
-Company ID: {loadedTicket.company['id']}""")
+Company: {companyname}
+Company ID: {companyid}""")
     lbl_description.config(text=loadedTicket.description_text)
 
 def closeTicket():
@@ -81,25 +94,28 @@ win_main.title("Freshdesk Ticket Utilities")
 frm_ticketlist = ttk.Frame(borderwidth=1, height=400, relief='raised', width=600)
 u0 = ttk.Label(frm_ticketlist, text='Company')
 u0.place(x=5, y=0)
-sel_company = ttk.Combobox(frm_ticketlist, values='""', width=16)
+sel_company = ttk.Combobox(frm_ticketlist, values='""', width=20)
 sel_company.place(x=5, y=20)
 u1 = ttk.Label(frm_ticketlist, text='From')
-u1.place(x=130, y=0)
+u1.place(x=150, y=0)
 ent_startdate = tkc.DateEntry(frm_ticketlist, locale="fi_FI", width=12)
-ent_startdate.place(x=130, y=20)
+ent_startdate.place(x=150, y=20)
+ent_startdate.set_date(datetime.today()-timedelta(days=7))
 u2 = ttk.Label(frm_ticketlist, text='Until')
-u2.place(x=230, y=0)
-ent_enddate = tkc.DateEntry(frm_ticketlist, locale="fi_Fi",width=12)
-ent_enddate.place(x=230, y=20)
-btn_refreshlist = ttk.Button(frm_ticketlist, text='Refresh and filter', command=getTicketList)
-btn_refreshlist.place(x=330, y=19)
+u2.place(x=250, y=0)
+ent_enddate = tkc.DateEntry(frm_ticketlist, locale="fi_Fi", width=12)
+ent_enddate.place(x=250, y=20)
+u3 = ttk.Label(frm_ticketlist, text="Status")
+u3.place(x=350, y=0)
+sel_statusfilter = ttk.Combobox(frm_ticketlist, values='"" "open" "pending" "resolved" "closed"', width=13)
+sel_statusfilter.place(x=350, y=20)
 btn_bulkclose = ttk.Button(frm_ticketlist, state='disabled', text='Close tickets on page', command=bulkClose)
 btn_bulkclose.place(x=5, y=45)
 confirmBulkCloseVar = tk.BooleanVar()
 chk_confirm = ttk.Checkbutton(frm_ticketlist, state='disabled', text='Are you sure?', variable=confirmBulkCloseVar, command=confirmBulkClose)
 chk_confirm.place(x=128, y=47)
-sel_statusfilter = ttk.Combobox(frm_ticketlist, values='"" "open" "pending" "resolved" "closed"', width=13)
-sel_statusfilter.place(x=330, y=45)
+btn_refreshlist = ttk.Button(frm_ticketlist, text='Refresh', command=getTicketList)
+btn_refreshlist.place(x=350, y=45)
 # Treeview configuration
 trv_ticketlist = ttk.Treeview(frm_ticketlist, height=14, columns=("Company", "Subject", "Status", "Created"), selectmode="browse", show="headings")
 trv_ticketlist.heading("#0", text="ID")
@@ -118,12 +134,12 @@ vsb_ticketlist.place(height=307, x=570, y=80)
 frm_ticketlist.pack(anchor='nw', side='top')
 # Ticket view frame
 frm_ticket = ttk.Frame(borderwidth=1, height=410, relief='raised', width=600)
-u3 = ttk.Label(frm_ticket, text='Description')
-u3.place(x=236, y=0)
+u4 = ttk.Label(frm_ticket, text='Description')
+u4.place(x=236, y=0)
 lbl_description = ttk.Label(frm_ticket, borderwidth=1, relief='sunken', wraplength=350)
 lbl_description.place(height=380, width=355, x=236, y=20)
-u4 = ttk.Label(frm_ticket, text='Ticket ID')
-u4.place(x=5, y=0)
+u5 = ttk.Label(frm_ticket, text='Ticket ID')
+u5.place(x=5, y=0)
 lbl_details = ttk.Label(frm_ticket)
 lbl_details.configure(text='''ID: 
 Subject: 
@@ -168,12 +184,12 @@ trv_ticketlist.bind("<Double-Button-1>", selectTicket)
 # Get companies
 companies = backend.getCompanies()
 for company in companies:
-    companyDict[company.name] = company.id
+    companyDict[company.id] = company.name
 
 # Construct company combobox options
 companyOptions = '"" '
 for company in companyDict:
-    companyOptions = companyOptions + f'"{company} ({companyDict[company]})" '
+    companyOptions = companyOptions + f'"{companyDict[company]} ({company})" '
 sel_company.config(values=companyOptions)
 
 # Start the main loop which in turn displays the window
